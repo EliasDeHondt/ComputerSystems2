e1d2f0deb33b875ef14af581162c4e02a3483af6
EliasDeHondt <Elias.dehondt@outlook.com>
2023-11-07 14:26:39 +0100

    Commit

---
+++ BashScripts/Tools/ColorSpectrum.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+######################
+# Van Elias De Hondt #
+######################
+# FUNCTIE: Kleuren
+
+
+## Basis Kleuren:
+reset='\e[0m'  # Reset naar de standaardkleur
+rood='\e[0;31m'  # Rood
+groen='\e[0;32m'  # Groen
+geel='\e[0;33m'  # Geel
+blauw='\e[0;34m'  # Blauw
+magenta='\e[0;35m'  # Magenta (Paars)
+cyaan='\e[0;36m'  # Cyaan (Blauwgroen)
+wit='\e[0;37m'  # Wit
+zwart='\e[0;30m'  # Zwart
+
+
+## Heldere Kleuren:
+helderrood='\e[1;31m'  # Helder Rood
+heldergroen='\e[1;32m'  # Helder Groen
+heldergeel='\e[1;33m'  # Helder Geel
+helderblauw='\e[1;34m'  # Helder Blauw
+heldermagenta='\e[1;35m'  # Helder Magenta
+heldercyaan='\e[1;36m'  # Helder Cyaan
+helderwit='\e[1;37m'  # Helder Wit
+
+## Achtergrondkleuren:
+bg_rood='\e[41m'  # Rode achtergrond
+bg_groen='\e[42m'  # Groene achtergrond
+bg_geel='\e[43m'  # Gele achtergrond
+bg_blauw='\e[44m'  # Blauwe achtergrond
+bg_magenta='\e[45m'  # Magenta achtergrond
+bg_cyaan='\e[46m'  # Cyaan achtergrond
+bg_wit='\e[47m'  # Witte achtergrond
--- BashScripts/Tools/SSH-Tool.sh
+++ BashScripts/Tools/SSH-Tool.sh
@@ -3,3 +3,19 @@
 # Van Elias De Hondt #
 ######################
 # FUNCTIE:
+reset='\e[0m'
+rood='\e[0;31m'
+
+
+
+function error_exit() {
+  echo -e "Error: ${rood}$1${reset}"
+  exit 1
+}
+
+
+read -p "Wat is de poort van de ssh server: " poort
+
+read -p "Wat is de user name van de ssh server: " username
+
+read -p "Wat is het ip van de ssh server: " ip
