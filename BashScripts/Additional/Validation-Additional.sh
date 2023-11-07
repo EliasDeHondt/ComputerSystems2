81123e473c5dd3ac4ce20008a6d040bdcb201e2d
EliasDeHondt <Elias.dehondt@outlook.com>
2023-11-07 16:29:07 +0100

    Commit

--- BashScripts/Additional/Validation-Additional.sh
+++ BashScripts/Additional/Validation-Additional.sh
@@ -7,107 +7,97 @@ reset='\e[0m'
 rood='\e[0;31m'
 
 
-# Functie: Error afhandeling.
-function error_exit() {
+# Possible comment -> (Ter informatie: Dit deel van de code werkt niet)
+# Exitcode 0: Dit geeft aan dat het commando of script met succes is uitgevoerd zonder fouten.
+# Exitcode 1-127: Dit geeft aan dat er problemen zijn opgetreden tijdens de uitvoering.
+# Exitcode 128+: Dit geeft aan dat het commando of script is beëindigd door een signaal. 
+# Bijvoorbeeld, 130 wordt vaak gebruikt wanneer een script wordt onderbroken door Ctrl+C (SIGINT).
+
+# echo "Exitcode: $?" # Dit geeft de exitcode van het laatst uitgevoerde commando weer
+
+function error_exit() { # Functie: Error afhandeling
   echo -e "Error: ${rood}$1${reset}"
   exit 1
 }
 
-# Functie: Controleer of het script als root wordt uitgevoerd.
-if [ "$EUID" -ne 0 ]; then
+if [ "$EUID" -ne 0 ]; then # Functie: Controleer of het script als root wordt uitgevoerd
   error_exit "Opstarten als root gebruiker: bv. sudo ./`basename $0`"
 fi
 
-# Zorgen dat het script een bepaald aantal argumenten verwacht
-if [ $# -ne 3 ]; then
+if [ $# -ne 3 ]; then # Functie: Zorgen dat het script een bepaald aantal argumenten verwacht
   error_exit "Ongeldig aantal argumenten. Gebruik: $0 arg1 arg2 arg3"
 fi
 
-# Controleren of een bestand bestaat
 bestandsnaam="voorbeeld.txt"
-if [ ! -f "$bestandsnaam" ]; then
+if [ ! -f "$bestandsnaam" ]; then # Functie: Controleren of een bestand bestaat
   error_exit "Bestand $bestandsnaam bestaat niet"
 fi
 
-# Controleren of de gebruikersinvoer binnen een specifiek bereik ligt
 read -p "Voer een getal in tussen 1 en 10: " gebruikersinvoer
-if ! [[ $gebruikersinvoer =~ ^[1-9]$|^10$ ]]; then
+if ! [[ $gebruikersinvoer =~ ^[1-9]$|^10$ ]]; then # Functie: Controleren of de gebruikersinvoer binnen een specifiek bereik ligt
   error_exit "Ongeldige invoer. Voer alstublieft een getal tussen 1 en 10 in"
 fi
 
-# Valideren van gebruikersbevestiging (J/N)
 read -p "Weet u het zeker? (J/N): " bevestiging
 bevestiging=${bevestiging^^} # Omzetten naar hoofdletters
-if [ "$bevestiging" != "J" ]; then
+if [ "$bevestiging" != "J" ]; then # Functie: Valideren van gebruikersbevestiging (J/N)
   error_exit "Actie geannuleerd."
 fi
 
-# Controleren of een directory bestaat
 map="voorbeeld_map"
-if [ ! -d "$map" ]; then
+if [ ! -d "$map" ]; then # Functie: Controleren of een directory bestaat
   error_exit "Map $map bestaat niet"
 fi
 
-# Zorgen dat een bestand de juiste rechten heeft
 bestand="voorbeeld_bestand.txt"
-if [ ! -w "$bestand" ]; then
+if [ ! -w "$bestand" ]; then # Functie: Zorgen dat een bestand de juiste rechten heeft
   error_exit "Bestand $bestand is niet beschrijfbaar"
 fi
 
-# Controleren op internetconnectiviteit
-if ! ping -c 1 google.com &> /dev/null; then
+if ! ping -c 1 google.com &> /dev/null; then # Functie: Controleren op internetconnectiviteit
   error_exit "Geen internetverbinding"
 fi
 
-# Controleren of een specifieke poort open is
 poort=80
-if ! nc -zv 127.0.0.1 $poort &> /dev/null; then
+if ! nc -zv 127.0.0.1 $poort &> /dev/null; then # Functie: Controleren of een specifieke poort open is
   error_exit "Poort $poort is niet open"
 fi
 
-# Controleren of een programma is geïnstalleerd
 required_program="nano"
-if ! command -v $required_program &> /dev/null; then
+if ! command -v $required_program &> /dev/null; then # Functie: Controleren of een programma is geïnstalleerd
   error_exit "Het programma '$required_program' is niet geïnstalleerd. Installeer dit programma om verder te gaan."
 fi
 
-# Controleren of een omgevingsvariabele is ingesteld
-if [ -z "$VAR_NAME" ]; then
+if [ -z "$VAR_NAME" ]; then # Functie: Controleren of een omgevingsvariabele is ingesteld
   error_exit "De omgevingsvariabele VAR_NAME is niet ingesteld"
 fi
 
-# Controleren of de invoer een geldig getal is
 read -p "Voer een getal in: " user_input
-if ! [[ $user_input =~ ^[0-9]+$ ]]; then
+if ! [[ $user_input =~ ^[0-9]+$ ]]; then # Functie: Controleren of de invoer een geldig getal is
   error_exit "Ongeldige invoer. Voer alstublieft een getal in"
 fi
 
-# Controleren op lege invoer
 read -p "Voer iets in: " user_input
-if [ -z "$user_input" ]; then
+if [ -z "$user_input" ]; then # Functie: Controleren op lege invoer
   error_exit "Er is niets ingevoerd"
 fi
 
-# Controleren of een gebruiker bestaat
 username="example_user"
-if ! id "$username" &>/dev/null; then
+if ! id "$username" &>/dev/null; then # Functie: Controleren of een gebruiker bestaat
   error_exit "De gebruiker $username bestaat niet"
 fi
 
-# Controleren of een groep bestaat
 groupname="example_group"
-if ! getent group "$groupname" &>/dev/null; then
+if ! getent group "$groupname" &>/dev/null; then # Functie: Controleren of een groep bestaat
   error_exit "De groep $groupname bestaat niet"
 fi
 
-# Controleren of een map leeg is
 directory="example_directory"
-if [ -n "$(find "$directory" -maxdepth 0 -type d -empty)" ]; then
+if [ -n "$(find "$directory" -maxdepth 0 -type d -empty)" ]; then # Functie: Controleren of een map leeg is
   error_exit "De map $directory is leeg"
 fi
 
-# Controleren van wachtwoordsterkte
 read -s -p "Voer een nieuw wachtwoord in: " password
-if [ ${#password} -lt 8 ]; then
+if [ ${#password} -lt 8 ]; then # Functie: Controleren van wachtwoordsterkte
   error_exit "Het wachtwoord moet minimaal 8 tekens lang zijn"
 fi
--- BashScripts/Tools/SSH-Tool.sh
+++ BashScripts/Tools/SSH-Tool.sh
@@ -5,17 +5,93 @@
 # FUNCTIE:
 reset='\e[0m'
 rood='\e[0;31m'
+blauw='\e[0;34m'
+line="############"
+Option1="Connection to remote server"
+Option2="Execute command on remote server"
+Option3="Upload data to remote server"
+Option4="View log page"
+logfile="/var/log/ssh-tool.log"
 
 
-
-function error_exit() {
+function error_exit() { # Functie: Error afhandeling
   echo -e "Error: ${rood}$1${reset}"
   exit 1
 }
 
+function main_menu() {
+  echo -e "${blauw}$line\n# SSH Tool #\n$line${reset}"
+  echo "1) $Option1"
+  echo "2) $Option2"
+  echo "3) $Option3"
+  echo "4) $Option4"
+  read -p "What do you want to do (1-4): " menu_decision
 
-read -p "Wat is de poort van de ssh server: " poort
+  if [[ ! $menu_decision =~ ^[1-4] ]]; then
+    error_exit "Invalid input. Please enter a number between 1 and 4"
+  fi
+}
 
-read -p "Wat is de user name van de ssh server: " username
+function basic_info() {
+  read -p "What is the port of the ssh server (Default 22): " port
+  if [[ $port == "" ]]; then
+    port=22
+  fi
+  if [[ $port -le 0 || $port -ge 65353 ]]; then
+    error_exit "The port number is not in the correct exclusive range 0-65353"
+  fi
 
-read -p "Wat is het ip van de ssh server: " ip
+  read -p "What is the user name of the ssh server (Required): " username
+  if [[ ! $username =~ ^[A-Za-z]+$ ]]; then
+    error_exit "The username can only consist of one of the 26 characters from the alphabet"
+  fi
+
+  read -p "What is the IP of the ssh server (Required): " ip
+  if [[ ! $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    error_exit "You did not enter a valid IP address (FQDN's are not allowed)"
+  fi
+}
+
+function make_log() {
+  echo -e "Performed action $1 $(date +'%Y-%m-%d %H:%M:%S')" >> "$logfile"
+}
+
+if [ "$EUID" -ne 0 ]; then # Functie: Controleer of het script als root wordt uitgevoerd
+  error_exit "Starting as root user: e.g. sudo ./`basename $0`"
+fi
+
+main_menu
+
+if [ $menu_decision -eq 4 ]; then
+  cat "$logfile"
+  make_log "$Option4"
+  exit 0
+fi
+
+basic_info
+
+if [ $menu_decision -eq 1 ]; then
+  ssh -p $port $username@$ip
+  make_log "$Option1"
+fi
+
+if [ $menu_decision -eq 2 ]; then
+  read -p "Which command do you want to use: " command
+  ssh -p $port $username@$ip $command
+  make_log "$Option2"
+fi
+
+if [ $menu_decision -eq 3 ]; then
+  read -p "Please enter the path of the file: " file
+  if [ ! -f "$file" ]; then # Functie: Controleren of een bestand bestaat
+    error_exit "File $file does not exist"
+  fi
+
+  read -p "In which directory should it be placed on the server: " directory
+  if [ ! -d "$directory" ]; then # Functie: Controleren of een directory bestaat
+    error_exit "Directory $directory does not exist"
+  fi
+
+  scp -P $port $file $username@$ip:$directory
+  make_log "$Option3"
+fi
