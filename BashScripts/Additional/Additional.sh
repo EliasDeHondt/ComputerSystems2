59d543ee3d8ce4773792bb164147bfc8f165b7c5
EliasDeHondt <Elias.dehondt@outlook.com>
2023-11-08 09:09:57 +0100

    Commit

---
+++ BashScripts/Additional/Additional.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+######################
+# Van Elias De Hondt #
+######################
+# Functie: Additional Additional :)
+
+# Possible comment -> (Ter informatie: Dit deel van de code werkt niet)
+# Exitcode 0: Dit geeft aan dat het commando of script met succes is uitgevoerd zonder fouten.
+# Exitcode 1-127: Dit geeft aan dat er problemen zijn opgetreden tijdens de uitvoering.
+# Exitcode 128+: Dit geeft aan dat het commando of script is beëindigd door een signaal. 
+# Bijvoorbeeld, 130 wordt vaak gebruikt wanneer een script wordt onderbroken door Ctrl+C (SIGINT).
+
+# echo "Exitcode: $?" # Dit geeft de exitcode van het laatst uitgevoerde commando weer
+
--- BashScripts/Additional/Validation-Additional.sh
+++ BashScripts/Additional/Validation-Additional.sh
@@ -7,14 +7,6 @@ reset='\e[0m'
 rood='\e[0;31m'
 
 
-# Possible comment -> (Ter informatie: Dit deel van de code werkt niet)
-# Exitcode 0: Dit geeft aan dat het commando of script met succes is uitgevoerd zonder fouten.
-# Exitcode 1-127: Dit geeft aan dat er problemen zijn opgetreden tijdens de uitvoering.
-# Exitcode 128+: Dit geeft aan dat het commando of script is beëindigd door een signaal. 
-# Bijvoorbeeld, 130 wordt vaak gebruikt wanneer een script wordt onderbroken door Ctrl+C (SIGINT).
-
-# echo "Exitcode: $?" # Dit geeft de exitcode van het laatst uitgevoerde commando weer
-
 function error_exit() { # Functie: Error afhandeling
   echo -e "Error: ${rood}$1${reset}"
   exit 1
