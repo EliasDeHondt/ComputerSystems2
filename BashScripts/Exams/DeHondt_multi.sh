cba49f2f3b34f7be446b3c87b1e4dc7ec0329318
EliasDeHondt <Elias.dehondt@outlook.com>
2023-11-09 12:31:14 +0100

    Commit

--- BashScripts/Exams/DeHondt_multi.sh
+++ BashScripts/Exams/DeHondt_multi.sh
@@ -8,14 +8,13 @@
 # Copyright: 2014 GNU v3 elias.dehondt@outlook.com
 # Versie: 0.1
 # Requires: Standaard shell find commando
-reset='\e[0m'
-rood='\e[0;31m'
-blauw='\e[0;34m'
-groen='\e[0;32m'
+reset=$'\e[0m'
+rood=$'\e[0;31m'
+blauw=$'\e[0;34m'
+groen=$'\e[0;32m'
 bestandsnaam="$1"
 required_program="bc"
 cpu_id=0
-
 
 function error_exit() {
   echo -e "Error: ${rood}$1${reset}"
@@ -23,20 +22,18 @@ function error_exit() {
 }
 
 function maxcpu() {
-  ps xo cpuid,cmd | grep -Eo " [0-9] " | grep -Eo "[0-9]" >> "temp.txt"
   max=0
   while IFS= read -r lijn; do
     if [ "$lijn" -gt "$max" ]; then
       max=$lijn
     fi
-  done < "temp.txt"
-  (( max+=1 ))
-  rm "temp.txt"
+  done < <(ps xo cpuid,cmd | grep -Eo " [0-9] " | grep -Eo "[0-9]")
+  (( max++ ))
 }
 
-# Start van het script
-if [ "$EUID" -eq 0 ]; then # Functie: Controleer of het script als root wordt uitgevoerd
-  error_exit "Opstarten als root mag niet vb. $0"
+# Start des Skripts
+if [ "$EUID" -ne 0 ]; then # Functie: Controleer of het script als root wordt uitgevoerd
+  error_exit "Opstarten als root mag niet vb. sudo $0"
 fi
 
 if [ ! -f "$bestandsnaam" ]; then # Functie: Controleren of een bestand bestaat
@@ -52,19 +49,17 @@ maxcpu
 echo -e "Deze computer heeft $max processoren"
 
 while IFS= read -r lijn; do
-  if [ ! "$lijn" = "" ]; then
+  if [ -n "$lijn" ]; then
+    berechnung=$(echo "$lijn" | grep -o '\".*\"' | sed -e 's/\"//g')
+    ergebnis=$(echo "$berechnung" | bc -l -q)
 
-    berekening_lijn=$(echo "$lijn" | grep -o "\\\".*\"" | sed -e "s/\\\"//;s/\"//")
-
-    berekening=$(echo $berekening_lijn | bc -l "berekening_lijn")
-
-    if [ "$cpu_id" = "$max" ]; then
-       cpu_id=0
-    else
-      command=$(taskset --cpu-list "$cpu_id" sh "${berekening}")
-      echo -e "COMMAND: $command"
-      echo -e "ANSWER: on cpu ${groen}$cpu_id${reset}:"
-      (( cpu_id+=1 ))
+    if [ "$cpu_id" -eq "$max" ]; then
+      cpu_id=0
     fi
+
+    command_to_execute="taskset --cpu-list $cpu_id sh <<< \"$ergebnis\""
+    echo -e "COMMAND: $command_to_execute"
+    echo -e "ANSWER: auf CPU ${groen}$cpu_id${reset}: $ergebnis"
+    (( cpu_id++ ))
   fi
 done < "$bestandsnaam"
