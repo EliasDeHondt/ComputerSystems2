f22f2d20f6babdc0a730439d26236836d2d18097
EliasDeHondt <Elias.dehondt@outlook.com>
2023-11-14 20:57:38 +0100

    Commit

--- DockerScripts/Additional/sqlserver.sh
+++ DockerScripts/Additional/sqlserver.sh
@@ -3,7 +3,11 @@
 # Van Elias De Hondt #
 ######################
 
+# https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-docker?view=sql-server-ver15&pivots=cs1-bash
 docker run -e 'ACCEPT_EULA=Y' -v 'sqldata:/var/opt/mssql' -e 'SA_PASSWORD=DB3&3DB&DB3' -p 1433:1433 --name sqlserver -d mcr.microsoft.com/mssql/server:2019-latest
+
+docker cp /home/elias/catchem2324.bak sqlserver:/tmp/catchem2324.bak # Copy file to container
+
 
 docker stop sqlserver
 docker start sqlserver
